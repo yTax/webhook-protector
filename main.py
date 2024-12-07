c295ed563a962631bd5a80b21abd72b57c5d9516
yTax <65450189+yTax@users.noreply.github.com>
2024-12-07 16:54:41 +0000

    debug=False

--- main.py
+++ main.py
@@ -45,4 +45,4 @@ def ratelimit_error(e):
     return jsonify(error="ratelimit exceeded", message=str(e.description)), 429
 
 if (__name__ == '__main__'):
-    app.run(debug=True, host='0.0.0.0', port=5000) # always keep debug=False when you deploy this
+    app.run(debug=False, host='0.0.0.0', port=5000) # always keep debug=False when you deploy this
